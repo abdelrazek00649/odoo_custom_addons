bb96efa868c8da724c77bc64a53bf4c3bc3fb890
abdelrazek00649 <Mohamedahmed06050@gmail.com>
2026-03-09 05:07:05 +0200

    Luxury Gold Theme - Official Upload

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Luxury Modern Invoice Gold',
+    'version': '1.0',
+    'category': 'Invoicing',
+    'summary': 'Premium luxury invoice theme with gold accents and boxed layout',
+    'price': 25.00,
+    'currency': 'USD',
+    'license': 'OPL-1',
+    'depends': ['account'],
+    'data': ['views/report_invoice.xml',],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
